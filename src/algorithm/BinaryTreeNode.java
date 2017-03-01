8035e093b4e395c0339e3ce72cc52313c79daacb
LotteryOne <xuanfengshizi@163.com>
2017-03-01 21:17:41 +0800

    binaryTree Test

---
+++ src/algorithm/BinaryTreeNode.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+/**
+ * Created by BlueSky on 2/25/2017.
+ */
+public class BinaryTreeNode {
+
+    public static int value;
+
+    public BinaryTreeNode leftNode;
+
+    public BinaryTreeNode rightNode;
+
+}
--- src/algorithm/Start.java
+++ src/algorithm/Start.java
@@ -2,6 +2,7 @@ package algorithm;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -10,7 +11,7 @@ import java.util.Stack;
 public class Start {
 
     /**
-     * 单项链表
+     * 单项链表 存取
      */
 
     @Test
@@ -35,6 +36,47 @@ public class Start {
         while (!stack.empty()) {
             System.out.println(stack.pop().data);
         }
+
+    }
+
+    /**
+     * 重建二叉树
+     */
+    @Test
+    public void buildBinaryTree() throws Exception {
+        int[] preSort = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] inSort = {4, 7, 2, 1, 5, 3, 8, 6};
+        BinaryTreeNode root = construcCore(preSort, inSort);
+//        System.out.println(root.value);
+    }
+
+    private BinaryTreeNode construcCore(int[] preSort, int[] inSort) throws Exception {
+
+        if (preSort == null || inSort == null) {
+            return null;
+        }
+        if (preSort.length != inSort.length) {
+            throw new Exception("长度不一致，无法重建");
+        }
+
+        BinaryTreeNode root = new BinaryTreeNode();
+
+        for (int i = 0; i < inSort.length; i++) {
+            if (inSort[i] == preSort[0]) {
+
+                root.value = inSort[i];
+                System.out.println(root.value);
+                root.leftNode = construcCore(Arrays.copyOfRange(preSort, 1, i + 1),
+                        Arrays.copyOfRange(inSort, 0, i));
+
+                root.rightNode = construcCore(Arrays.copyOfRange(preSort, i + 1, preSort.length),
+                        Arrays.copyOfRange(inSort, i + 1, inSort.length));
+
+            }
+        }
+
+
+        return root;
 
     }
 
